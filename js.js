d9366ce00a304191f044c9fb829d0ccf1b4a221a
luket1521 <61922273+luket1521@users.noreply.github.com>
2020-04-03 21:47:24 -0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,16 @@
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 80 ||
+    document.documentElement.scrollTop > 80
+  ) {
+    document.getElementById("navbar").style.padding = "15px 0px";
+    document.getElementById("logo").style.fontSize = "25px";
+  } else {
+    document.getElementById("navbar").style.padding = "60px 0px";
+    document.getElementById("logo").style.fontSize = "35px";
+  }
+}
